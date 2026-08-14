82b612de1e7dc26352641dffb44080f7c59cab21
Diego <70644302+NrtypmForhp@users.noreply.github.com>
2026-08-14 14:01:41 +0200

    Add files via upload

---
+++ upgrade_mongodb_docker_container.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e # Exit immediately if a command exits with a non-zero status
+SERVICE_NAME="mongodb"
+CONTAINER_NAME="mongodb_docker_container"
+TEMPORARY_BACKUP_DIRECTORY_NAME="temp_mongodb_backup"
+
+mkdir -p $TEMPORARY_BACKUP_DIRECTORY_NAME # Temporary directory to store backup and docker yml file
+
+echo "-*-* Create docker compose yml file *-*-"
+cat >$TEMPORARY_BACKUP_DIRECTORY_NAME/docker-compose.yml <<EOL
+services:
+  mongodb:
+    image: mongodb/mongodb-atlas-local:latest
+    container_name: mongodb_docker_container
+    hostname: mongodb
+    restart: always
+    ports:
+      - "27017:27017"
+    volumes:
+      - atlas_data:/data/db
+      - atlas_config:/data/configdb
+      - atlas_search_data:/data/mongot
+
+volumes:
+  atlas_data:
+  atlas_config:
+  atlas_search_data:
+EOL
+
+echo "-*-* Starting mongodb docker container upgrade *-*-"
+echo "Create backup inside docker container"
+docker exec $CONTAINER_NAME mongodump --uri="mongodb://localhost:27017/?directConnection=true" --out=/tmp/backup
+
+echo "Create a local copy of database from docker container"
+docker cp $CONTAINER_NAME:/tmp/backup $TEMPORARY_BACKUP_DIRECTORY_NAME/
+
+read -p "Check local folder now, and be sure the local copy of the backup is present! Starting to upgrade (and delete docker container)? (y/N): " confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    echo "Stop docker container"
+else
+    echo "User stopped database backup. EXIT!!"
+    exit 0
+fi
+
+docker stop "$CONTAINER_NAME"
+
+echo "Remove old container"
+docker rm -f $CONTAINER_NAME
+
+echo "Recreate docker container with upgraded version"
+docker compose -f $TEMPORARY_BACKUP_DIRECTORY_NAME/docker-compose.yml up -d --force-recreate
+
+echo "Copy local backup directory inside new created docker container"
+docker cp $TEMPORARY_BACKUP_DIRECTORY_NAME/backup $CONTAINER_NAME:/tmp/backup
+
+echo "Restore the backup"
+docker exec $CONTAINER_NAME mongorestore --drop --uri="mongodb://localhost:27017/?directConnection=true" /tmp/backup
+
+echo "Backup restored"
+read -p "Check your database now. Delete backup files? (y/N): " confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    echo "Deleting local and container backup files"
+    
+    echo "Delete backup inside the container"
+    docker exec mongodb_docker_container rm -rf /tmp/backup
+    
+    echo "Delete local backup folder"
+    rm -rf $TEMPORARY_BACKUP_DIRECTORY_NAME
+    
+    echo "Cleanup complete!"
+else
+    echo "Cleanup skipped. Backup files retained."
+    exit 0
+fi
+
+echo "Finished!"
